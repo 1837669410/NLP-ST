fbdcc9d4954f12d15ab9c41a91125ad7d54dbaec
zhangtianwei <1837669410@qq.com>
2023-04-20 16:30:46 +0800

    add set gpu code

---
+++ utils.py
@@ -0,0 +1,10 @@
+def set_soft_gpu(soft_gpu):
+    import tensorflow as tf
+    if soft_gpu:
+        gpus = tf.config.experimental.list_physical_devices('GPU')
+        if gpus:
+            # Currently, memory growth needs to be the same across GPUs
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
